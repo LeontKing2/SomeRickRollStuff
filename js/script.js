3d5d68351215c71ab9186e2b5ee6af07bb872a99
DESKTOP-PVQB0H3\abhia <abhistreamslive@gmail.com>
2021-10-17 13:05:24 +0530

    Added No Code rickroll teleporting to therickroll.com

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ document.getElementById("RickRollBtn").onclick = function() {
     //Redirect to rick astley rick roll
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 }
+
+document.getElementById("NoCodeRRBtn").onclick = function() {
+    //Say that Get Rick Rolled Idiot and then redirect Redirect to therickroll.com
+    console.log("Get Rick Rolled Idiot")
+    //Redirect to therickroll.com It is made by github.com/PwnFunction Pls merge my pull requests xdd
+    window.location.replace("https://therickroll.com")
+
+}
