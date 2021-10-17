71ebcfe18d0bdb3915d5dbe14f3604464fda927a
DESKTOP-PVQB0H3\abhia <abhistreamslive@gmail.com>
2021-10-17 12:54:49 +0530

    'Epic'

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("RickRollBtn").onclick = function() {
+    //Say that Get Rick Rolled Idiot and then redirect to rick astley rickroll
+    console.log("Get Rick Rolled Idiot")
+    //Redirect to rick astley rick roll
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
